058be5395a035eb610effd90fffe8bb05b7f7974
Didier Hobak <dual.didier.hobak@gmail.com>
2022-06-12 11:14:29 -0500

    Inicio de proyecto

---
+++ FixReportSystem/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FixReportSystem.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
